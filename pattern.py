704fd4093753465eec75f98eedcfa0f454bafe7f
rishika553 <rishikabangwal02@gmail.com>
2026-07-17 12:25:51 +0530

    solve 12 to 17 pattern question

--- pattern.py
+++ pattern.py
@@ -55,7 +55,54 @@ class Solution:
             for j in range(i+1):
                 print(start,end="")
                 start=1-start
+
            
+
+        for i in range (1,n+1):
+           #left number
+           for j in range(1,i+1):
+               print(j,end="")
+            #space
+           for j in range(2*(n-1)):
+               print(" ",end="")
+            #right number 
+
+           for j in range(i,0,-1):
+               print(j,end="")
+
+           print()       
+                
+
+        num=1
+        for i in range(1,n+1):
+
+            for j in range(1,i+1):
+                print(num,end="")
+                num+=1
             print()
+
+
+        for i in range(n+1,-1,-1):
+
+            for char in range(1,i+1):
+                print(chr(ord('A') + char-1 ),end="")
+            print()
+
+        for i in range(1,n+1):
+            for char in range(1,i+1):
+                print(chr(ord('A')+ i -1),end="")
+            print()  
+
+        for i in range(1,n+1):
+            #space
+            for j in range(n-i,0,-1):
+                print(" ",end='')
+            #left 
+            for j in range(1,i+1):
+                print(chr(ord('A') +j-1),end="")
+            #right
+            for j in range(i-1,0,-1):
+                print(chr(ord('A')+j-1),end="")
+            print()    
 obj=Solution()
 obj.pattern1(5)
